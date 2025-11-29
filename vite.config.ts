f28dcaa75e8c453210cde677cb2a60ad7ac355a8
GISETZ <souralehkouame53@gmail.com>
2025-11-29 20:28:47 +0000

    ayaoue

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       "@shared": path.resolve(__dirname, "./shared"),
     },
   },
-  root: ".",
+  root: path.resolve(__dirname),
   server: {
     middlewareMode: true,
   },
